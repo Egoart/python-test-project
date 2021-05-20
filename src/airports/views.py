0cb60ceec559014fcd619f9ae053399a99cc59c2
Egor <egor.4r@gmail.com>
2021-05-20 16:41:22 +0300

    Add airports dataset

--- src/airports/views.py
+++ src/airports/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .airtports_base import AIRPORTS_CODES
 
 # Create your views here.
 
-AIRPORTS_CODES = {
-    'DXB': 'DUBAI',
-    'HAJ':'HANNOVER',
-    'HAN':'HANOI',
-    'LGW': 'LONDON GATWICK',
-    'LHR': 'LONDON HEATHROW',
-    'LAX': 'LOS ANGELES',
-    'ORY': 'PARIS ORLY',
-    'ROM': 'ROME',
-    'SIN': 'SINGAPORE-CHANGI',
-    'VNO': 'VILNIUS' 
-}
+# AIRPORTS_CODES = {
+#     'DXB': 'DUBAI',
+#     'HAJ':'HANNOVER',
+#     'HAN':'HANOI',
+#     'LGW': 'LONDON GATWICK',
+#     'LHR': 'LONDON HEATHROW',
+#     'LAX': 'LOS ANGELES',
+#     'ORY': 'PARIS ORLY',
+#     'ROM': 'ROME',
+#     'SIN': 'SINGAPORE-CHANGI',
+#     'VNO': 'VILNIUS' 
+# }
 
 def airports(request, code):
     city = AIRPORTS_CODES.get(code.upper(), 'UNKNOWN')
