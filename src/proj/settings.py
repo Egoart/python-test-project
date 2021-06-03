70c77ea7c82cc9c605f2a221de2291ccffa6febd
Egor <egor.4r@gmail.com>
2021-06-03 11:33:54 +0300

    Modify settings.py

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -124,6 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/media/'                                                                                                                                              
+ 
+STATIC_ROOT = ''
+MEDIA_ROOT = ''
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
